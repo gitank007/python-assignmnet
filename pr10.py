7b33099522b30a7bb5b8588e67195b66a5b21c32
gitank007 <45426043+gitank007@users.noreply.github.com>
2021-01-30 00:03:58 +0530

    left over assignmnet of assignmnet 10

---
+++ pr10.py
@@ -0,0 +1,6 @@
+#finding the hcf 
+x=int(input("enter the first value of which you want to fidn the hcf :  "))
+y=int(input("enter the secoond value for which you want to find the hcf  : "))
+import math
+print(f"the hcf of the given values {x,y} is ",math.gcd(x,y))
+1
