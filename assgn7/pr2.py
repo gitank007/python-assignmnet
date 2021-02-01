d83805fbbcd3fc993547adb49a3ebbf6d524afbe
gitank007 <45426043+gitank007@users.noreply.github.com>
2021-02-02 00:07:44 +0530

    pr1.py

--- assgn7/pr2.py
+++ assgn7/pr2.py
@@ -1,2 +1,10 @@
-x=(input())
+x=(input("enter the length of the string "))
+# printing the lenght of the stting using the len opeartor 
 print(len(x))
+# however it an string and it is itearble so we can use a count varibel and incresing the val of count while iterating over string 
+count=0
+for in x:
+  count+=1
+print(count)
+
+
