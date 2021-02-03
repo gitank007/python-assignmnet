523dd9c13742f3f1cb8c8992be0438e09642b519
gitank007 <45426043+gitank007@users.noreply.github.com>
2021-02-03 22:56:40 +0530

    Next Prime no

--- assgn4/pr2.py
+++ assgn4/pr2.py
@@ -13,3 +13,15 @@ while True:
         print("The next prime No is ",x)
         break
 
+        
+# methode 2 prime n0 
+i=2
+x=int(input("enter the number fow which you want to check the next prime no :"))
+x+=1
+while i!=x:
+    if x%i==0:
+        x+=1
+    else:
+        i+=1
+print("The Next Prime NO is :",x)
+
