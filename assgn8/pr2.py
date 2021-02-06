219b852340f6d74393d7df0633bd03c9fd546fa1
gitank007 <45426043+gitank007@users.noreply.github.com>
2021-02-07 00:11:11 +0530

    sorting the values in the tuple  using  the inbuilt methode sorted

--- assgn8/pr2.py
+++ assgn8/pr2.py
@@ -1,2 +1,3 @@
-l=4,5,3,2,1
-sorted(l)
+# taking input and storing as a tuple and after that sorting using the sorted function what is this do is return  the sorted Tupletupfd_
+tup_l=eval(input("enter the tuple  values in seprating with the space: ")
+sorted(tup_l) #no need to print the value of it as the sorted function is already gonna print that 
